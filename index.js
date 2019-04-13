76b5ad5e9177d337929daa3a2671a8425cceed09
Victor Leandro <samuellv010@hotmail.com>
2019-04-13 15:40:27 -0300

    contrucao

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const ig = require('./instagram');
-const user = require('./user')
+const user = require('./user');
 
 (async() =>{
 
     await ig.initialize();
-    
+    debugger;
     await ig.login(user.USERNAME,user.PASSWORD);
-  
+
+    await ig.likeTagsProcess(['ibm','developer','datascience','java','nerd','geek']);
+
 })()
